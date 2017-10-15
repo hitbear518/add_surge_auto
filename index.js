d2c715e96057daa7d5155ee646b24db344ddda77
Sen Wang <hitbear518@gmail.com>
2017-10-15 16:22:46 +0800

    feat: add prompt and options

--- index.js
+++ index.js
@@ -1,23 +1,67 @@
 const axios = require('axios');
 const fs = require('fs');
+const inquirer = require('inquirer');
+const program = require('commander');
+program
+  .option('--url [subscription url]', 'subscription url')
+  .option('--group [proxy group name]', 'proxy group name')
+  .option('--write-to-iCloud', 'write to iCloud Documents if set ', false)
+  .parse(process.argv);
 
-const subscriptionUrl = process.env.SURGE_SUBSCRIPTION; // replace with your subscriptioin url
-const mainGroupName = process.env.SURGE_MAIN_GROUP; // replace with your group name
-const escapedSubscription = subscriptionUrl.replace('?', '\\?');
 (async function() {
-  const { data: originalConf } = await axios.get(subscriptionUrl);
-  const mainGroup = originalConf.match(new RegExp(`${mainGroupName} = .*`))[0];
-  const autoGroup = mainGroup
-    .replace(mainGroupName, 'Auto')
-    .replace('select,', 'url-test,url = http://www.gstatic.com/generate_204,');
-  const newGroups =
-    mainGroup.replace('select,', 'select,Auto,') + '\n' + autoGroup;
-  const confWithAuto = originalConf
-    .replace(new RegExp(`.*${escapedSubscription}.*`), '')
-    .replace(mainGroup, newGroups);
-  fs.writeFileSync(
-    `${process.env
-      .HOME}/Library/Mobile Documents/iCloud~run~surge/Documents/surge_with_auto.conf`,
-    confWithAuto,
-  );
+  try {
+    let subscriptionUrl = program.url;
+    if (!subscriptionUrl) {
+      ({ subscriptionUrl } = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'subscriptionUrl',
+          message: 'Enter your subscription link',
+        },
+      ]));
+    }
+    const { data: originalConf } = await axios
+      .get(subscriptionUrl)
+      .catch(error => {
+        throw new Error(
+          'Download configuration file failed, please check your subscription url and try later',
+        );
+      });
+    let mainGroupName = program.group;
+    if (!mainGroupName) {
+      ({ mainGroupName } = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'mainGroupName',
+          message: 'Enter your proxy group name',
+        },
+      ]));
+    }
+    let mainGroup;
+    try {
+      mainGroup = originalConf.match(new RegExp(`${mainGroupName} = .*`))[0];
+    } catch (error) {
+      throw new Error(`Can't find your proxy group of name ${mainGroupName}`);
+    }
+    const autoGroup = mainGroup
+      .replace(mainGroupName, 'Auto')
+      .replace(
+        'select,',
+        'url-test,url = http://www.gstatic.com/generate_204,',
+      );
+    const newGroups =
+      mainGroup.replace('select,', 'select,Auto,') + '\n' + autoGroup;
+    const escapedSubscription = subscriptionUrl.replace('?', '\\?');
+    const confWithAuto = originalConf
+      .replace(new RegExp(`.*${escapedSubscription}.*`), '')
+      .replace(mainGroup, newGroups);
+    const writePath = program.writeToICloud
+      ? `${process.env
+          .HOME}/Library/Mobile Documents/iCloud~run~surge/Documents/${mainGroupName}_with_auto.conf`
+      : require('path').resolve(__dirname, `${mainGroupName}_with_auto.conf`);
+    fs.writeFileSync(writePath, confWithAuto);
+    console.log(`Success, check your new configuration file at: ${writePath}`);
+  } catch (error) {
+    console.log(error);
+  }
 })();
